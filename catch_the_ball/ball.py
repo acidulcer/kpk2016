c59751cbaae0382056983e1e25c469dfa85b5ae6
acidulcer <acid_ulcer@inbox.ru>
2016-07-14 17:59:26 +0300

    Добвлен подсчет очков при клике по шарику 1) если нажата ЛКМ и цвет объекта равен "цвету для ЛКМ", то увеличиваем количество очков на 1, иначе уменьшаем на 1  2) если нажата СКМ и цвет объетка на совпадает с "цветом для ЛКМ" и не совпадает с "цветом для ПКМ", то увеличиваем количество очков на 1, иначе уменьшаем на 1 3) если нажата ПКМ и цвет объекта равен "цвету для ПКМ", то увеличиваем количество очков на 1, иначе уменьшаем на 1

--- catch_the_ball/ball.py
+++ catch_the_ball/ball.py
@@ -6,18 +6,67 @@ ball_minimal_radius = 15
 ball_maximal_radius = 40
 ball_available_colors = ['green', 'blue', 'red', 'lightgray', '#FF00FF', '#FFFF00']
 
+def score_up():
+    """
+    увеличивает число набранных очков на 1
+
+    текущее значение очков берет из глобпльной переменной score
+    :return:
+    """
+    global score
+    score_current = score.get()
+    score_current += 1
+    score.set(score_current)
+
+def score_down():
+    """
+    уменьшает число набранных очков на 1
+
+    текущее значение очков берет из глобпльной переменной score
+    :return:
+    """
+    global score
+    score_current = score.get()
+    score_current -= 1
+    score.set(score_current)
+
 def click_ball(event):
     """ Обработчик событий мышки для игрового холста canvas
     :param event: событие с координатами клика
-    По клику мышкой нужно удалять тот объект, на который мышка указывает.
-    А также засчитываеть его в очки пользователя.
+    По клику мышки определяет цвет кликнутого объетка, и:
+        1) если нажата ЛКМ и цвет объекта равен "цвету для ЛКМ",
+           то увеличиваем количество очков на 1, иначе уменьшаем на 1
+        2) если нажата СКМ и цвет объетка на совпадает с "цветом для ЛКМ" и не совпадает с "цветом для ПКМ",
+           то увеличиваем количество очков на 1, иначе уменьшаем на 1
+        3) если нажата ПКМ и цвет объекта равен "цвету для ПКМ",
+           то увеличиваем количество очков на 1, иначе уменьшаем на 1
+
+    По клику мышки удаляется тот объект, на который мышка указывает.
     """
+    global score
     obj = canvas.find_closest(event.x, event.y)
     x1, y1, x2, y2 = canvas.coords(obj)
 
     if x1 <= event.x <= x2 and y1 <= event.y <= y2:
+        if event.num == 1:
+            if canvas.itemconfig(obj)['fill'][4] == label_lkm_color['text']:
+                score_up()
+            else:
+                score_down()
+        elif event.num == 2:
+            if (canvas.itemconfig(obj)['fill'][4] != label_lkm_color['text']) \
+                    and (canvas.itemconfig(obj)['fill'][4] != label_pkm_color['text']):
+                score_up()
+            else:
+                score_down()
+        else:
+            if canvas.itemconfig(obj)['fill'][4] == label_pkm_color['text']:
+                score_up()
+            else:
+                score_down()
+
         canvas.delete(obj)
-        # FIXME: нужно учесть объект в очках
+
         create_random_ball()
 
 
@@ -55,13 +104,38 @@ def init_ball_catch_game():
         create_random_ball()
 
 def init_main_window():
-    global root, canvas
+    global root, canvas, label, score, label_lkm_color, label_pkm_color
 
     root = tkinter.Tk()
+
+    label_lkm_text = tkinter.Label(root,  text="Цвет для ЛКМ")
+    label_lkm_text.pack()
+    cvet_fona_lkm = random_color()
+    label_lkm_color = tkinter.Label(root,  text=str(cvet_fona_lkm), bg=cvet_fona_lkm)
+    label_lkm_color.pack()
+
+    label_score_text = tkinter.Label(root,  text="Набранные очки")
+    label_score_text.pack()
+    score = tkinter.IntVar(0)
+    label = tkinter.Label(root,  text="Очки: ", textvariable=score)
+    label.pack()
+
+    label_pkm_text = tkinter.Label(root,  text="Цвет для ПКМ")
+    label_pkm_text.pack()
+    cvet_fona_pkm = random_color()
+    while cvet_fona_pkm == cvet_fona_lkm:
+        cvet_fona_pkm = random_color()
+    label_pkm_color = tkinter.Label(root,  text=str(cvet_fona_pkm), bg=cvet_fona_pkm)
+    label_pkm_color.pack()
+
     canvas = tkinter.Canvas(root, background='white', width=400, height=400)
     canvas.bind("<Button>", click_ball)
     canvas.bind("<Motion>", move_all_balls)
     canvas.pack()
+
+
+
+
 
 
 if __name__ == "__main__":
